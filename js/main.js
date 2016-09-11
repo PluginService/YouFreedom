1ddccb64e38df213fdafe34dc5182f8a837660b5
aggelosQQ <jouflis1@hotmail.com>
2016-09-11 13:44:44 +0300

    Collapsible Categories

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+   $(".one").click(function() {
+      $(".general").slideToggle("slow");
+   });
+   $(".two").click(function() {
+      $(".promo").slideToggle("slow");
+   });
+   $(".three").click(function() {
+      $(".training").slideToggle("slow");
+   });
+   $(".four").click(function() {
+      $(".streaming").slideToggle("slow");
+   });
+});
